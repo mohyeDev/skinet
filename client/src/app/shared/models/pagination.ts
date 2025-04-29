0cd5efbd48fd483f3406832f0f5aca1acf4481fc
Mohye <Mohye20@gmail.com>
2025-04-29 06:36:32 +0300

    Using Types in our Project

---
+++ client/src/app/shared/models/pagination.ts
@@ -0,0 +1,8 @@
+export type Pagination<T> = {
+
+    pageIndex : number ;
+    pageSize : number ; 
+    count : number  ;
+    data : T[]
+
+}
---
+++ client/src/app/shared/models/product.ts
@@ -0,0 +1,12 @@
+export type Product = {
+
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    pictureUrl: string;
+    type: string;
+    brand: string;
+    quantityInStock: number;
+
+}
