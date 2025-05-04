73939a9b2c67be76396a7269668b92c14b903dfd
Mohye <Mohye20@gmail.com>
2025-05-04 07:27:48 +0300

    Using a class to supply the parameters for the API request

--- client/src/app/core/services/shop.service.ts
+++ client/src/app/core/services/shop.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Pagination } from '../../shared/models/pagination';
 import { Product } from '../../shared/models/product';
+import { ShopParams } from '../../shared/models/shopParams';
 
 @Injectable({
   providedIn: 'root'
@@ -12,22 +13,23 @@ export class ShopService {
   types : string[] = []; 
   brands : string[] = []
   
-  getProducts(brands?:string[] , types?:string[] , sort?:string) {
+  getProducts(shopParams : ShopParams) {
     let params = new HttpParams();
-    if(brands && brands.length >0){
-      params =params.append('brands',brands.join(','))
+    
+    if(shopParams.brands && shopParams.brands.length >0){
+      params =params.append('brands',shopParams.brands.join(','))
 
     }
 
-    if(types && types.length>0)
+    if(shopParams.types && shopParams.types.length>0)
     {
-      params = params.append('types' , types.join(','))
+      params = params.append('types' , shopParams.types.join(','))
     }
 
 
-    if(sort)
+    if(shopParams.sort)
     {
-      params = params.append('sort' , sort);
+      params = params.append('sort' , shopParams.sort);
     }
 
     params =   params.append('pageSize',20);
--- client/src/app/features/shop/shop.component.ts
+++ client/src/app/features/shop/shop.component.ts
@@ -9,6 +9,7 @@ import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import {MatMenu, MatMenuTrigger} from '@angular/material/menu'
 import { MatListOption, MatSelectionList, MatSelectionListChange } from '@angular/material/list';
+import { ShopParams } from '../../shared/models/shopParams';
 
 @Component({
   selector: 'app-shop',
@@ -30,15 +31,15 @@ export class ShopComponent implements OnInit{
   private shopService= inject(ShopService);
   private dialogService = inject(MatDialog);
   products : Product[] = [];
-  selectedBrands : string[] = [] ;
-  selectedTypes :string [] = [] ;
-  selectedSort : string = 'name';
+
   sortOptions = [
     {name : "Alphabetical",value : 'name'},
     {name : "Price: Low-High",value : 'priceAsc'},
     {name : "Price: High-Low",value : 'PriceDesc'}
 
   ]
+
+  shopParams = new ShopParams();
 
   ngOnInit(): void {
      this.initalizeShop();
@@ -54,7 +55,7 @@ export class ShopComponent implements OnInit{
 
   getProduct()
   {
-    this.shopService.getProducts(this.selectedBrands,this.selectedTypes,this.selectedSort).subscribe(
+    this.shopService.getProducts(this.shopParams).subscribe(
       {
         next : response => this.products = response.data,
         error: error => console.log(error)
@@ -68,7 +69,7 @@ export class ShopComponent implements OnInit{
   const selectedOption = event.options[0];
   if(selectedOption)
   {
-    this.selectedSort = selectedOption.value ; 
+    this.shopParams.sort = selectedOption.value ; 
     this.getProduct();
     
   }
@@ -78,24 +79,22 @@ export class ShopComponent implements OnInit{
     const dialogRef = this.dialogService.open(FiltersDialogComponent,{
        minWidth: "500px" ,
        data : {
-        selectedBrands : this.selectedBrands,
-        selectedTypes : this.selectedTypes
+        selectedBrands : this.shopParams.brands,
+        selectedTypes : this.shopParams.types 
        }
     });
     dialogRef.afterClosed().subscribe(
       {
         next: result => {
           if(result){
-            this.selectedBrands = result.selectedBrands;
-            this.selectedTypes = result.selectedTypes ; 
+            this.shopParams.brands = result.selectedBrands;
+            this.shopParams.types = result.selectedTypes ; 
 
-            this.shopService.getProducts(this.selectedBrands , this.selectedTypes).subscribe(
-              {
-                next: response => this.products = response.data,
-                error: error => console.log(error)
-                
-              }
-            )
+            this.shopService.getProducts(this.shopParams).subscribe({
+              next: response => this.products = response.data,
+              error: error => console.log(error)
+              
+            });
             
           }
         }
