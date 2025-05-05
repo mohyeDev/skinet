020a51e1638dc2b040c64725fa28bfb0ae4d1adf
mohyeDev <mohye20@gmail.com>
2025-05-05 19:03:17 +0300

    Persisting the cart

--- client/src/app/core/services/cart.service.ts
+++ client/src/app/core/services/cart.service.ts
@@ -3,6 +3,7 @@ import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Cart, CartItem } from '../../shared/models/cart';
 import { Product } from '../../shared/models/product';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -17,9 +18,12 @@ export class CartService {
 
   getCart(id:string)
   {
-    return this.http.get<Cart>(this.baseUrl + 'cart?id=' + id).subscribe({
-      next: cart => this.cart.set(cart)
-    });
+    return this.http.get<Cart>(this.baseUrl + 'cart?id=' + id).pipe(
+      map(cart => {
+        this.cart.set(cart);
+        return cart ;
+      })
+    )
   }
 
   setCart(cart : Cart)
