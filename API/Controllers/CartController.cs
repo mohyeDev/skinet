8c80fec35ad64bc485e9c48f0f0b684f7f863803
mohyeDev <mohye20@gmail.com>
2025-05-05 09:47:51 +0300

    Creating the Cart controller

---
+++ API/Controllers/CartController.cs
@@ -0,0 +1,38 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class CartController(ICartService cartService) : BaseApiController
+{
+    [HttpGet]
+
+    public async Task<ActionResult<ShoppingCart>> GetCardById(string id)
+    {
+        var cart = await cartService.GetCartAsync(id);
+        return Ok(cart ?? new ShoppingCart{Id = id});
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ShoppingCart>> UpdateCart(ShoppingCart cart){
+
+        var updatedCart =await cartService.SetCartAsync(cart);
+        if(updatedCart is null) return BadRequest("Problem With Cart!");
+        return updatedCart;
+
+    }
+
+    [HttpDelete]
+
+    public async Task<ActionResult> DeleteCart(string id)
+    {
+        var result =  await cartService.DeleteCartAsync(id);
+
+        if(!result ) return BadRequest("Problem Deleting Cart!");
+
+        return Ok();
+
+    }
+
+}
