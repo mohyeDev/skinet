3bc00e73872477e7f59a838ca401d6d3c6c81346
mohyeDev <mohye20@gmail.com>
2025-05-06 13:35:05 +0300

    Updating the Header Component

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -3,9 +3,10 @@ import {MatBadge} from '@angular/material/badge';
 import { Component, inject } from '@angular/core';
 import {MatButton} from '@angular/material/button';
 import {MatProgressBar} from '@angular/material/progress-bar'
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { BusyService } from '../../core/services/busy.service';
 import { CartService } from '../../core/services/cart.service';
+import { AccountService } from '../../core/services/account.service';
 
 @Component({
   selector: 'app-header',
@@ -26,5 +27,18 @@ export class HeaderComponent {
 
   busyService = inject(BusyService);
   cartService = inject(CartService);
+  accountService = inject(AccountService);
+  private router = inject(Router);
+
+  logout(){
+    this.accountService.logout().subscribe({
+      next : ()=>{
+        this.accountService.currentUser.set(null);
+        this.router.navigateByUrl('/');
+      }
+    });
+  }
+
+  
 
 }
