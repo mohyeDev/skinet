1b94be4fb2453d1c7d813e7f8e8c73385c8ddbbc
mohyeDev <mohye20@gmail.com>
2025-05-06 08:05:44 +0300

    Creating extension methods

---
+++ API/Extensions/ClaimsPrincipleExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Authentication;
+using System.Security.Claims;
+using Core.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions;
+
+public static class ClaimsPrincipleExtensions
+{
+    public static async Task<AppUser> GetUserByEmail(this UserManager<AppUser> userManager , ClaimsPrincipal user)
+    {
+        var userToReturn = await userManager.Users.FirstOrDefaultAsync(x=> 
+        x.Email == user.GetEmail() );
+
+        if(userToReturn is null) throw new AuthenticationException("User Not Found");
+
+        return userToReturn;
+    }
+
+    public static string GetEmail(this ClaimsPrincipal user)
+    {
+        var email = user.FindFirstValue(ClaimTypes.Email) 
+        ?? throw new AuthenticationException("Email Claim Not Found");
+        return email ; 
+    }
+}
