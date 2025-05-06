ae7c16053944a3f931f6d11a2a7488318046311c
mohyeDev <mohye20@gmail.com>
2025-05-06 05:58:30 +0300

    Creating the checkout components

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -6,44 +6,49 @@ import { TestErrorComponent } from './features/test-error/test-error.component';
 import { NotFoundComponent } from './shared/component/not-found/not-found.component';
 import { ServerErrorComponent } from './shared/component/server-error/server-error.component';
 import { CartComponent } from './features/cart/cart.component';
+import { CheckoutComponent } from './features/checkout/checkout.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: HomeComponent
-    },
-    {
-        path: 'shop',
-        component: ShopComponent
-    },
-    {
-        path: 'shop/:id',
-        component: ProductDetailsComponent
-    },
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'shop',
+    component: ShopComponent,
+  },
+  {
+    path: 'shop/:id',
+    component: ProductDetailsComponent,
+  },
 
-    {
-        path : 'test-error',
-        component:TestErrorComponent
-    },
-    {
-        path : 'not-found',
-        component:NotFoundComponent
-    },
+  {
+    path: 'test-error',
+    component: TestErrorComponent,
+  },
+  {
+    path: 'not-found',
+    component: NotFoundComponent,
+  },
 
-    {
-        path:'server-error',
-        component:ServerErrorComponent
-    },
+  {
+    path: 'server-error',
+    component: ServerErrorComponent,
+  },
 
-    {
-        path:'cart',
-        component:CartComponent
-    },
+  {
+    path: 'cart',
+    component: CartComponent,
+  },
 
-    
-    {
-        path: '**',
-        redirectTo: 'not-found',
-        pathMatch: 'full'
-    }
+  {
+    path: 'checkout',
+    component: CheckoutComponent,
+  },
+
+  {
+    path: '**',
+    redirectTo: 'not-found',
+    pathMatch: 'full',
+  },
 ];
