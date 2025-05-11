57027a4a6a274c0ecad195888d335927295f49a0
MohyeDev <mohye20@gmail.com>
2025-05-11 10:07:14 +0300

    Createing The Order Controller

---
+++ API/Controllers/OrderController.cs
@@ -0,0 +1,79 @@
+﻿using API.Dtos;
+using API.Extensions;
+using Core.Entities;
+using Core.Entities.OrderAggregate;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Authorize]
+public class OrderController (ICartService cartService , IUnitOfWork unit): BaseApiController
+{
+
+    [HttpPost]
+
+    public async Task<ActionResult<Order>> CreateOrder(CreateOrderDto orderDto){
+
+        var email = User.GetEmail();
+
+        var cart = await cartService.GetCartAsync(orderDto.CartId );
+
+        if(cart is null) return BadRequest("Cart Not Found!");
+
+        if(cart.PaymentIntentId is null) return BadRequest("No Payment Intent for this order!");
+
+        var items = new List<OrderItem>();
+
+        foreach(var item in cart.Items){
+
+            var productItem = await unit.Repository<Product>().GetByIdAsync(item.ProductId) ; 
+
+            if(productItem is null) return BadRequest("Problem with the order!");
+
+            var itemOrdered = new ProductItemOrdered{
+                ProductId = item.ProductId,
+                ProductName = item.ProductName,
+                PictureUrl = item.PictureUrl ,
+
+            };
+
+            var orderItem = new OrderItem{
+                ItemOrdered = itemOrdered,
+                Price = productItem.Price,
+                Quantity = item.Quantity
+            };
+
+            items.Add(orderItem);
+
+
+        }
+
+
+        var deliveryMethod = await unit.Repository<DeliveryMethod>().GetByIdAsync(orderDto.DeliveryMethodId);
+
+        if(deliveryMethod is null ) return BadRequest("No Delivery Method Selected!");
+
+        var order =new Order{
+            OrderItems = items,
+            DeliveryMethod = deliveryMethod,
+            ShippingAddress = orderDto.shippingAddress,
+            Subtotal = items.Sum(x => x.Price * x.Quantity),
+            PaymentSummary = orderDto.paymentSummary,
+            PaymentIntentId = cart.PaymentIntentId,
+            BuyerEmail = email
+
+        };  
+
+        unit.Repository<Order>().Add(order); 
+
+        if(await unit.Complete()){
+            return order;
+        }
+
+        return BadRequest("Problem Creating Order!");
+
+    }
+
+}
---
+++ API/Dtos/CreateOrderDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Core.Entities.OrderAggregate;
+
+namespace API.Dtos;
+
+public class CreateOrderDto
+{
+
+    [Required]
+    public string CartId {get;set;}= string.Empty;
+    [Required]
+
+    public int DeliveryMethodId {get;set;}
+    [Required]
+
+    public ShippingAddress shippingAddress {get;set;} = null! ; 
+    [Required]
+
+    public PaymentSummary paymentSummary {get;set;} = null ! ;
+
+}
