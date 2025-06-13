704114341f1d6a6d1d2213b3de434a0125767c14
mohyeDev <mohye20@gmail.com>
2025-06-13 16:14:55 +0300

    Updating the checkout success page

--- client/src/app/features/checkout/checkout-success/checkout-success.component.ts
+++ client/src/app/features/checkout/checkout-success/checkout-success.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
+import { SignalrService } from '../../../core/services/signalr.service';
+import { CurrencyPipe, DatePipe, NgIf } from '@angular/common';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { AddressPipe } from '../../../shared/pipes/address.pipe';
+import { PaymentCardPipe } from '../../../shared/pipes/payment-card.pipe';
 
 @Component({
   selector: 'app-checkout-success',
   standalone: true,
-  imports: [MatButton ,RouterLink],
+  imports: [MatButton ,RouterLink,CurrencyPipe,MatProgressSpinnerModule , DatePipe,CurrencyPipe,AddressPipe,PaymentCardPipe,NgIf],
   templateUrl: './checkout-success.component.html',
   styleUrl: './checkout-success.component.scss'
 })
 export class CheckoutSuccessComponent {
+
+  signalrService = inject(SignalrService);
+
+
+
 
 }
